4622b68829abda997b6a2b14df4b66ea08452d6d
Kwame Porter Robinson <kporterrobinson@gmail.com>
2024-02-15 18:06:24 -0500

    (feat) dockerizes and instruements Marker, for converting pdfs to .md files

--- to_markdown.sh
+++ to_markdown.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-PDF_DIR="~/Downloads/theoretical and paradigmatic committments"
-MARKDOWN_DIR="~/Documents/thesis/Paper 1 - Computation for Community-based Economies"
+PDF_DIR="."
+MARKDOWN_DIR="."
 
 # Convert to absolute paths and handle directories with spaces
 PDF_DIR_SANITIZED=$(eval echo "$PDF_DIR" | sed 's/ /\\ /g' | xargs realpath)
 MARKDOWN_DIR_SANITIZED=$(eval echo "$MARKDOWN_DIR" | sed 's/ /\\ /g' | xargs realpath)
 
-docker run -v "$PDF_DIR_SANITIZED":/pdfs dibz15/marker_docker:latest python convert_single.py /pdfs/hook-2015-knowledge-production.pdf /pdfs/hook-2015-knowledge-production.md --parallel_factor 2 --max_pages 10
+cd marker && ./build_me.sh && cd ..
 
-mv *.md $MARKDOWN_DIR
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+
+docker run --shm-size=10.24gb --gpus all -v "$PDF_DIR_SANITIZED":/pdfs --user $USER_ID:$GROUP_ID marker:latest python convert.py /pdfs/ /pdfs/
+
+#sudo chown $(id -u):$(id -g) *.md
+#sudo chown $(id -u):$(id -g) *.json
+
+#mv *.md $MARKDOWN_DIR
+#rm *_meta.json
