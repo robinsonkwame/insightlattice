e82a6e8613d1f98540e652d6a94a32af52c9d152
Kwame Porter Robinson <kporterrobinson@gmail.com>
2024-02-16 10:15:33 -0500

    (wip) udpates filetypes to ignore

--- to_markdown.sh
+++ to_markdown.sh
@@ -12,6 +12,6 @@ cd marker && ./build_me.sh && cd ..
 USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 
-#docker run --shm-size=10.24gb --gpus all -v "$PDF_DIR_SANITIZED":/pdfs --user $USER_ID:$GROUP_ID marker:latest python convert.py /pdfs/ /pdfs/
+docker run --shm-size=10.24gb --gpus all -v "$PDF_DIR_SANITIZED":/pdfs --user $USER_ID:$GROUP_ID marker:latest python convert.py /pdfs/ /pdfs/
 
 mv $PDF_DIR_SANITIZED/*.md $MARKDOWN_DIR
