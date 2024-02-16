8c37730319e7ff134259587485ea723050ad87b8
Kwame Porter Robinson <kporterrobinson@gmail.com>
2024-02-16 10:07:23 -0500

    (fix) makes pdf, md directories local to repo, more self contained

--- to_markdown.sh
+++ to_markdown.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PDF_DIR="."
-MARKDOWN_DIR="."
+PDF_DIR="./pdf"
+MARKDOWN_DIR="./md"
 
 # Convert to absolute paths and handle directories with spaces
 PDF_DIR_SANITIZED=$(eval echo "$PDF_DIR" | sed 's/ /\\ /g' | xargs realpath)
@@ -12,10 +12,6 @@ cd marker && ./build_me.sh && cd ..
 USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 
-docker run --shm-size=10.24gb --gpus all -v "$PDF_DIR_SANITIZED":/pdfs --user $USER_ID:$GROUP_ID marker:latest python convert.py /pdfs/ /pdfs/
+#docker run --shm-size=10.24gb --gpus all -v "$PDF_DIR_SANITIZED":/pdfs --user $USER_ID:$GROUP_ID marker:latest python convert.py /pdfs/ /pdfs/
 
-#sudo chown $(id -u):$(id -g) *.md
-#sudo chown $(id -u):$(id -g) *.json
-
-#mv *.md $MARKDOWN_DIR
-#rm *_meta.json
+mv $PDF_DIR_SANITIZED/*.md $MARKDOWN_DIR
